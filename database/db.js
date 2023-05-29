b88d3dd90219d1b956dac018c6e2fff5648eeaa1
harshit_sharma <sharmaharshit769@gmail.com>
2023-05-29 17:53:37 +0530

    localhost

--- database/db.js
+++ database/db.js
@@ -1,7 +1,11 @@
 const mongoose = require("mongoose");
 // require("dotenv").config();
 // const 
-const link = "mongodb://localhost:27017/Agro_hackathon"
+// const link = "mongodb://localhost:27017/Agro_hackathon"
+// const link = "mongodb+srv://anuragtiwari:anurag1234@clusterfortnpproject.2ri4uvj.mongodb.net/tnpProjectDB?retryWrites=true&w=majority"
+const link ="mongodb+srv://database:database@cluster0.z0wy1h3.mongodb.net/june4thyearproject?retryWrites=true&w=majority"
+
+
 // const link = process.env.DATABASE
 const connectToMongo = () => {
   mongoose
--- index.js
+++ index.js
@@ -2,10 +2,11 @@ const express = require("express");
 const app = express();
 const connectToMongo = require("./database/db");
 const cors = require("cors");
+const port = process.env.PORT || 5000
 // require("dotenv").config();
 app.use(cors());
 // const port = 5000
-const port=  5000
+// const port=  5000
 // to use req.body
 app.use(express.json());
 
