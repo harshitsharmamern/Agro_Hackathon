0fb6ea5358b9d9ea8154d20c8244093aa5da29bb
harshit_sharma <sharmaharshit769@gmail.com>
2023-05-29 17:54:17 +0530

    resolve page,home

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import './App.css';
 import Home from './Component/Home'
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Signup from './Component/Helper/Signup';
+import Page from './Component/Page';
 
 function App() {
   return (<>
@@ -10,7 +11,7 @@ function App() {
     <Router>
       <Routes>
 
-        <Route path='/'element={<Home />} />
+        <Route path='/'element={<Page />} />
         <Route path='/home'element={<Home />} />
         <Route path='/signup'element={<Signup />} />
 
---
+++ src/Component/Page.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import Navbar from './Helper/Navbar'
+const Page = () => {
+  return (
+    <>
+     <Navbar/>
+     page
+    </>
+  )
+}
+
+export default Page
